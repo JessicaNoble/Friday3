ba610b9095f6d2166b2cab336839d4f944005738
JessicaNoble <jessica.nble@gmnail.com>
2022-05-27 14:35:29 +1200

    First commit.

---
+++ Friday3.js
@@ -0,0 +1,23 @@
+//variabels (let, const, var)
+
+
+//initialize
+let a = 10;
+let b; 
+//re-declaration
+let c = "apple";
+//let c = "banana"
+
+let a = 10;
+
+function house() {
+  let a = 20; //re-declaration
+    console.log("1", a)
+}
+
+console.log("2", a)
+
+house():
+
+
+console.log("3",a)
